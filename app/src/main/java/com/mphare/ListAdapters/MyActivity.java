736c8fb4ae14b3b9adc31c5ceeee8a9b8cf1f242
Michael Hare <mhare@zixcorp.com>
2015-06-22 09:58:01 -0500

    Initial commit

---
+++ app/src/main/java/com/mphare/ListAdapters/MyActivity.java
@@ -0,0 +1,25 @@
+package com.mphare.ListAdapters;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MyActivity extends Activity
+{
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+
+  public void gotoListView(View view)
+  {
+    Intent intent = new Intent(this, ListView1.class);
+    startActivity(intent);
+  }
+}
